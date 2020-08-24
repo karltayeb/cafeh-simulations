8df08e1c25a7b9b4e3401157edddeab0f3194d05
Karl Tayeb <karl.tayeb7@gmail.com>
2020-08-24 18:05:57 -0400

    Revert "remove tpfptnfn from score"
    This reverts commit aa4c4d929e1bff724b7efb582e4b4149430a1c5a.

--- code/scoring.py
+++ code/scoring.py
@@ -22,7 +22,11 @@ def score_coloc_cafeh(active, purity, true_coloc, thresh=0.9, filter_purity=Fals
     model_coloc = np.concatenate(
         [[p_coloc(t1, t2) for t1 in range(t2)] for t2 in range(n_study)])
     return {
-        'p_coloc': model_coloc
+        'p_coloc': model_coloc,
+        'true_positive': (true_coloc & (model_coloc > thresh)).sum(),
+        'false_positive': (~true_coloc & (model_coloc > thresh)).sum(),
+        'true_negative': (~true_coloc & ~(model_coloc > thresh)).sum(),
+        'false_negative': (true_coloc & ~(model_coloc > thresh)).sum()
     }
 
 
@@ -33,7 +37,11 @@ def score_coloc_coloc(coloc_out, true_coloc, thresh=0.9):
     p_coloc = np.concatenate(
         [[coloc_out[(t1, t2)].pph4 for t1 in range(t2)] for t2 in range(n_study)])
     return {
-        'p_coloc': p_coloc
+        'p_coloc': p_coloc,
+        'true_positive': (true_coloc & (p_coloc > thresh)).sum(),
+        'false_positive': (~true_coloc & (p_coloc > thresh)).sum(),
+        'true_negative': (~true_coloc & ~(p_coloc > thresh)).sum(),
+        'false_negative': (true_coloc & ~(p_coloc > thresh)).sum()
     }
 
 
@@ -44,7 +52,11 @@ def score_coloc_ecaviar(ecaviar_out, true_coloc, thresh=0.9):
     p_coloc = np.concatenate(
         [[ecaviar_out[(t1, t2)].max() for t1 in range(t2)] for t2 in range(n_study)])
     return {
-        'p_coloc': p_coloc
+        'p_coloc': p_coloc,
+        'true_positive': (true_coloc & (p_coloc > thresh)).sum(),
+        'false_positive': (~true_coloc & (p_coloc > thresh)).sum(),
+        'true_negative': (~true_coloc & ~(p_coloc > thresh)).sum(),
+        'false_negative': (true_coloc & ~(p_coloc > thresh)).sum()
     }
 
 
